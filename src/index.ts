1e541b3f6dfe42f45e9897a569cbc02b4a108443
fogelo <fogelo@yandex.ru>
2024-01-04 12:42:51 +0300

    favicon

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,10 @@ app.get("/", (req, res) => {
   res.json("hello world");
 });
 
+app.get("/favicon.ico", (req, res) => {
+  res.json("hello world");
+});
+
 app.listen(port, () => {
   console.log(`server is running on ${port} port`);
 });
